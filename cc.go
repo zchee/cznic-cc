d6121f9cfe089c0fd79742948bd5e966f25432ba
Steffen <steffen.butzer@outlook.com>
2017-04-26 03:34:59 +0200

    win: fix generate to work for HostCppConfig. References #58

--- cc.go
+++ cc.go
@@ -30,6 +30,7 @@ import (
 	"go/token"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
 
 	"github.com/cznic/golex/lex"
@@ -197,19 +198,24 @@ func HostConfig(opts ...string) (predefined string, includePaths, sysIncludePath
 // Execution of HostConfig is not free, so caching the results is recommended
 // whenever possible.
 func HostCppConfig(cpp string, opts ...string) (predefined string, includePaths, sysIncludePaths []string, err error) {
-	args := append(append([]string{"-dM"}, opts...), "/dev/null")
+	args := append(append([]string{"-dM"}, opts...), os.DevNull)
 	pre, err := exec.Command(cpp, args...).Output()
 	if err != nil {
 		return "", nil, nil, err
 	}
 
-	args = append(append([]string{"-v"}, opts...), "/dev/null")
+	args = append(append([]string{"-v"}, opts...), os.DevNull)
 	out, err := exec.Command(cpp, args...).CombinedOutput()
 	if err != nil {
 		return "", nil, nil, err
 	}
 
-	a := strings.Split(string(out), "\n")
+	sep := "\n"
+	if runtime.GOOS == "windows" {
+		sep = "\r\n"
+	}
+
+	a := strings.Split(string(out), sep)
 	for i := 0; i < len(a); {
 		switch a[i] {
 		case "#include \"...\" search starts here:":
